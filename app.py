aef2753bbf59a010cde20e24ffc492c14f5c1b87
seanstertm <sean.patrick.richards@gmail.com>
2022-07-15 20:50:59 -0500

    creating cta service

--- app.py
+++ app.py
@@ -1,41 +1,51 @@
 from flask import Flask, render_template, request
 import os
 import configparser
-import requests
+import dbus
 
 # set up Flask app
 app = Flask(__name__)
 
 # get the root directory
 dir = os.path.dirname(__file__)
+
 # get path to options file
 filename = os.path.join(dir, 'options.ini')
+
 # set up config file
 config = configparser.ConfigParser()
 config.read(filename)
 
+
+sysbus = dbus.SystemBus()
+systemd1 = sysbus.get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
+manager = dbus.Interface(systemd1, 'org.freedesktop.systemd1.Manager')
+
 # load website when accessed
 @app.route("/")
 def home():
-    mode = int(config['MAIN']['mode'])
-    return render_template('index.html', mode = mode, bus_text = "N/A")
+    mode = config['MAIN']['mode']
 
-# handle mode
-@app.route("/mode", methods=["POST"])
+    if mode == 'cta':
+        manager.StartUnit('ctabus.service', 'replace')
+
+    return render_template('index.html', mode = mode)
+
+@app.route("/cta", methods=["POST"])
 def toggle_mode():
-    print(request.form["mode"])
-    mode = int(request.form["mode"])
+    config.set("MAIN", "mode", "cta")
 
-    response = requests.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=Yr9nbHrDBVbxtdC4j8BTw5R3z&rt=152&stpid=12472&format=json")
-    bus_json = response.json()
-    bus_text = "Next busses: "
-    if "error" in bus_json['bustime-response']:
-        return render_template('index.html', mode = mode, bus_text = "No busses in the next 30 minutes")
-    for bus in bus_json['bustime-response']['prd']:
-        if bus_text != "Next busses: ":
-            bus_text = bus_text + ", "
-        bus_text = bus_text + bus['prdctdn'] + " mins"
+    manager.StartUnit('ctabus.service', 'replace')
 
-    return render_template('index.html', mode = mode, bus_text = bus_text)
+    return render_template('index.html', mode = 'cta')
+
+@app.route("/spotify", methods=["POST"])
+def spotify():
+    config.set("MAIN", "mode", "spotify")
+
+    manager.StopUnit('ctabus.service', 'replace')
+
+    return render_template('index.html', mode = 'spotify')
+
 
 app.run(host='0.0.0.0', port=80)
---
+++ ctabus.py
@@ -0,0 +1,29 @@
+import sys
+import time
+import requests
+
+route = 152
+stop = 12472
+
+prevTime = ""
+nowTime = ""
+
+try:
+    while True:
+        try:
+            response = requests.get(f"http://ctabustracker.com/bustime/api/v2/getpredictions?key=Yr9nbHrDBVbxtdC4j8BTw5R3z&rt={route}&stpid={stop}&format=json")
+            bus_json = response.json()
+            if "error" in bus_json['bustime-response']:
+                nowTime = "No busses in the next 30 minutes"
+            else: 
+                nowTime = bus_json['bustime-response']['prd'][0]['prdctdn']
+            if prevTime != nowTime:
+                print(f"--\nctabus.service: {nowTime}\n--")
+            prevTime = nowTime
+            time.sleep(5)
+        except Exception as e:
+            print(e)
+            time.sleep(1)
+except KeyboardInterrupt:
+    print("stopping...")
+    sys.exit(0)
