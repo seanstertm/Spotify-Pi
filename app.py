2eb08b86d3d44981461cacc78853fc20e310bb73
seanstertm <sean.patrick.richards@gmail.com>
2022-07-16 10:52:01 -0500

    created remote control

--- app.py
+++ app.py
@@ -21,31 +21,67 @@ sysbus = dbus.SystemBus()
 systemd1 = sysbus.get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
 manager = dbus.Interface(systemd1, 'org.freedesktop.systemd1.Manager')
 
-# load website when accessed
 @app.route("/")
 def home():
     mode = config['MAIN']['mode']
+    power = config['MAIN']['power']
+    brightness = config['MAIN']['brightness']
 
     if mode == 'cta':
         manager.StartUnit('ctabus.service', 'replace')
 
-    return render_template('index.html', mode = mode)
+    return render_template('index.html', mode = mode, power = power, brightness = brightness)
 
 @app.route("/cta", methods=["POST"])
 def toggle_mode():
     config.set("MAIN", "mode", "cta")
+    brightness = config['MAIN']['brightness']
 
     manager.StartUnit('ctabus.service', 'replace')
 
-    return render_template('index.html', mode = 'cta')
+    return render_template('index.html', mode = 'cta', power = 'on', brightness = brightness)
 
 @app.route("/spotify", methods=["POST"])
 def spotify():
     config.set("MAIN", "mode", "spotify")
+    brightness = config['MAIN']['brightness']
 
     manager.StopUnit('ctabus.service', 'replace')
 
-    return render_template('index.html', mode = 'spotify')
+    return render_template('index.html', mode = 'spotify', power = 'on', brightness = brightness)
+
+@app.route("/brightness", methods=["POST"])
+def brightness():
+    brightness = request.form['brightness']
+    config.set("MAIN", "brightness", brightness)
+
+    mode = config['MAIN']['mode']
+
+    if mode == 'cta':
+        manager.RestartUnit('ctabus.service', 'fail')
+
+    return render_template('index.html', mode = mode, power = 'on', brightness = brightness)
+
+@app.route("/poweroff", methods=['POST'])
+def poweroff():
+    config.set("MAIN", "power", "off")
+    mode = config['MAIN']['mode']
+
+    if mode == 'cta':
+        manager.StopUnit('ctabus.service', 'replace')
+
+    return render_template('index.html', power = 'off')
+
+@app.route('/poweron', methods=['POST'])
+def poweron():
+    config.set("MAIN", "power", "on")
+    mode = config['MAIN']['mode']
+    brightness = config['MAIN']['brightness']
+
+    if mode == 'cta':
+        manager.StartUnit('ctabus.service', 'replace')
+
+    return render_template('index.html', power = 'on', mode = mode, brightness = brightness)
 
 
 app.run(host='0.0.0.0', port=80)
