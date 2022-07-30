1d49044ca81b8c26b856d4003109423fb5abdc74
seanstertm <sean.patrick.richards@gmail.com>
2022-07-29 22:56:33 -0500

    revised test file

--- ledMatrixTest.py
+++ ledMatrixTest.py
@@ -1,5 +1,7 @@
 from PIL import Image
 import os
+import time
+import sys
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 
 dir = os.path.dirname(__file__)
@@ -12,7 +14,7 @@ options.chain_length = 1
 options.parallel = 1
 options.hardware_mapping = "regular"
 options.gpio_slowdown = 0
-options.brightness = 100
+options.brightness = 50
 options.limit_refresh_rate_hz = 60
 
 matrix = RGBMatrix(options = options)
@@ -21,3 +23,9 @@ image = Image.open(testImage)
 image.thumbnail((32, 32), Image.ANTIALIAS)
 
 matrix.SetImage(image.convert('RGB'))
+
+try:
+    while True:
+        time.sleep(100)
+except KeyboardInterrupt:
+    sys.exit(0)
